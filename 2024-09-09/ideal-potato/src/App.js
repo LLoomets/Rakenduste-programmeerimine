dba7a59b18331e4d60666a73f7b1f81bd37c0888
Liisi Loomets <loomets@tlu.ee>
2024-09-15 12:44:16 +0300

    feat: quick start practice

--- 2024-09-09/ideal-potato/src/App.js
+++ 2024-09-09/ideal-potato/src/App.js
@@ -7,9 +7,36 @@ import Show from './components/Show';
 import Context from "./components/Context";
 
 function App() {
-  const [show, setShow] = useState(true)
+  const [show, setShow] = useState(true);
 
-  const toggleShow = () => setShow(prevShow => !prevShow)
+  const toggleShow = () => setShow(prevShow => !prevShow);
+
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl : "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize :90,
+  };
+
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'green'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
   return (
     <>
@@ -21,6 +48,23 @@ function App() {
       <Counter />
       <Name title="Liisi" />
       <Name />
+
+      <h3>{user.name}</h3>
+      <img 
+        className = "avatar"
+        src = {user.imageUrl}
+        alt = {"Photo of " + user.name}
+        style = {{
+          width : user.imageSize,
+          height : user.imageSize,
+        }}
+      />
+
+      <ul>{listItems}</ul>
+
+      <button onClick={handleClick}>
+        Click me
+      </button>
     </>
   );
 }
