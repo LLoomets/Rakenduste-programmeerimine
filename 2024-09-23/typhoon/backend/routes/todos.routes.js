7b13a43231b18af4a74877bf0c84d03d0bddc747
Liisi Loomets <loomets@tlu.ee>
2024-10-03 13:11:07 +0300

    feat: express-validator

--- 2024-09-23/typhoon/backend/routes/todos.routes.js
+++ 2024-09-23/typhoon/backend/routes/todos.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const todosController = require("../controllers/todo.controller");
 const {
@@ -6,11 +7,36 @@ const {
   todosGetRouteMiddleware,
 } = require("../middlewares/todos.middlewares");
 
-router.use(todosRouteMiddleware)
+router.use(todosRouteMiddleware);
+
+const validateTodo = [
+  check("title")
+    .isString()
+    .withMessage("Title must be a string")
+    .notEmpty()
+    .withMessage("Title is required"),
+  check("priority")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Priority must be an integer between 1 and 5"),
+];
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 router.get("/", todosGetRouteMiddleware, todosController.read);
-router.post("/", todosController.create);
-router.put("/", todosController.update);
-router.delete("/:id", todosController.delete);
+router.post("/", validateTodo, handleValidationErrors, todosController.create);
+router.put("/", validateTodo, handleValidationErrors, todosController.update);
+
+router.delete(
+  "/:id",
+  check("id").isNumeric().withMessage("ID must be a number"),
+  handleValidationErrors,
+  todosController.delete
+);
 
 module.exports = router;
